33309a0af1b39edf37b3ad651cb0ed785df7e118
xmnh <fss1550837448@vip.qq.com>
2020-12-10 11:31:35 +0800

    用户登录注册

---
+++ pdlg-dao/src/main/java/com/zzj/vo/User.java
@@ -0,0 +1,67 @@
+package com.zzj.vo;
+
+public class User {
+    private Integer uid;
+    private String username;
+    private String password;
+    private String uname;
+    private String sex;
+    private String phone;
+    private String address;
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ pdlg-service/src/main/java/com/zzj/service/UserServiceImpl.java
@@ -0,0 +1,47 @@
+package com.zzj.service;
+
+import com.github.pagehelper.PageHelper;
+import com.zzj.dao.UserDao;
+import com.zzj.vo.PageVo;
+import com.zzj.vo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+    @Autowired
+    UserDao userDao;
+    @Override
+    public User login(String username, String password) {
+        return userDao.login(username,password);
+    }
+
+    @Override
+    public int register(User user) {
+        return userDao.register(user);
+    }
+
+    @Override
+    public int updateUser(User user) {
+        return userDao.updateUser(user);
+    }
+
+    @Override
+    public User queryUserById(int id) {
+        return userDao.queryUserById(id);
+    }
+
+    @Override
+    public PageVo<User> queryAllUser(User user, int page, int rows) {
+        PageVo<User> pageVo = new PageVo<User>();
+        PageHelper.startPage(page,rows);
+        pageVo.setRows(userDao.queryAllUser(user));
+        pageVo.setTotal(userDao.queryUserByCount(user));
+        return pageVo;
+    }
+
+    @Override
+    public User queryUserName(String username) {
+        return userDao.queryUserName(username);
+    }
+}
